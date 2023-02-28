461a4c0ac3ea285ae0ef72044c603df64c29d7cb
Maher <maher.fettah@gmail.com>
2023-02-28 19:19:02 +0100

    step 2 : print todo list

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'red',
+  },
+  bold: {
+    fontWeight: 'bold'
+  }
+});
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      color: 'red',
-    },
-    bold: {
-      fontWeight: 'bold'
-    }
-  });
+const sampleGoals = [
+  "Faire les courses",
+  "Aller à la salle de sport 3 fois par semaine",
+  "Monter à plus de 5000m d altitude",
+  "Acheter mon premier appartement",
+  "Perdre 5 kgs",
+  "Gagner en productivité",
+  "Apprendre un nouveau langage",
+  "Faire une mission en freelance",
+  "Organiser un meetup autour de la tech",
+  "Faire un triathlon",
+];
+
+export default function App() {
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text style = {styles.title}>Liste des <Text style = {styles.bold}>Tâches</Text> :</Text>
+      <View>
+        <Text style = {styles.title}>Liste des <Text style = {styles.bold}>Tâches</Text> :</Text>
+      </View>
+      <View>
+        {sampleGoals.map((goal) => (<Text>{goal}</Text>))}
+      </View>
     </View>
   );
 }
